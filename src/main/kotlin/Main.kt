601d5f541476a3d4d9605d055c8d0889a4752555
kboeckler <kevinboeckler@gmail.com>
2023-12-21 07:38:44 +0100

    Follow radar and report afterwards

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 import java.util.*
 import kotlin.math.abs
 
+const val SIZE = 10_000
+
 fun main(args: Array<String>) {
     val input = Scanner(System.`in`)
     val creatureCount = input.nextInt()
@@ -15,7 +17,7 @@ fun main(args: Array<String>) {
 
     while (true) {
         val myDrones = mutableListOf<Drone>()
-        val creatures = mutableListOf<Creature>()
+        val creatures = mutableMapOf<Int, Creature>()
         val creaturesSaved = mutableSetOf<Int>()
         val blips = mutableListOf<Blip>()
 
@@ -60,7 +62,7 @@ fun main(args: Array<String>) {
             val creatureY = input.nextInt()
             val creatureVx = input.nextInt()
             val creatureVy = input.nextInt()
-            creatures.add(Creature(creatureId, V(creatureX, creatureY), V(creatureVx, creatureVy)))
+            creatures[creatureId] = Creature(creatureId, V(creatureX, creatureY), V(creatureVx, creatureVy))
         }
         val radarBlipCount = input.nextInt()
         for (i in 0 until radarBlipCount) {
@@ -70,26 +72,33 @@ fun main(args: Array<String>) {
             blips.add(Blip(droneId, creatureId, RadarDir.valueOf(radar)))
         }
         val state = State(myDrones, creatures, scannedCreatures, lightCountByDrone, blips)
-        val output = calculate(state)
-        println(output.joinToString("\n"))
+        val result = calculate(state)
+        scannedCreatures = result.state.scannedCreatures.toMutableSet()
+        lightCountByDrone = result.state.lightCountByDrone.toMutableMap()
+        println(result.output.joinToString("\n"))
     }
 }
 
 data class State(
     val myDrones: List<Drone>,
-    val creatures: List<Creature>,
+    val creatures: Map<Int, Creature>,
     val scannedCreatures: Set<Int>,
     val lightCountByDrone: Map<Int, Int>,
     val blips: List<Blip>
 )
 
+data class Result(val output: List<String>, val state: State)
+
 /**
  *  MOVE <x> <y> <light (1|0)> | WAIT <light (1|0)>
  */
-private fun calculate(state: State): List<String> {
+private fun calculate(state: State): Result {
     val myLightCountByDrone = state.lightCountByDrone.toMutableMap()
     val myScannedCreates = state.scannedCreatures.toMutableSet()
     val output = mutableListOf<String>()
+
+    state.creatures.values.map { it.id }.forEach { myScannedCreates.add(it) }
+
     for (i in state.myDrones.indices) {
         val drone = state.myDrones[i]
         val light = myLightCountByDrone[i]!! > 0
@@ -99,25 +108,32 @@ private fun calculate(state: State): List<String> {
         val lightNum = if (light) "1" else "0"
         val creatureToScan = closestBy(
             drone,
-            blipCoords(drone, state.blips).filterNot { myScannedCreates.contains(it.second.id) }) { it.first }
+            blipCoords(
+                drone,
+                state.blips
+            ).filterNot { myScannedCreates.contains(it.second) }) { it.first }
         if (drone.battery >= 15) {
-            // TODO mark this globally
             myLightCountByDrone[i] = 3
         }
         if (creatureToScan != null) {
-            // TODO mark scanned creatures globally by seeing them
-            myScannedCreates.add(creatureToScan.second.id)
             output.add("MOVE ${creatureToScan.first.x} ${creatureToScan.first.y} $lightNum")
         } else {
-            output.add("WAIT $lightNum")
+            output.add("MOVE ${drone.c.x} 0 $lightNum")
         }
     }
-    return output
+    val resultingState = state.copy(lightCountByDrone = myLightCountByDrone, scannedCreatures = myScannedCreates)
+    return Result(output, resultingState)
 }
 
-private fun blipCoords(drone: Drone, blips: List<Blip>): List<Pair<V, Creature>> {
-    // TODO
-    return emptyList()
+fun blipCoords(drone: Drone, blips: List<Blip>): List<Pair<V, Int>> {
+    val coordsByDir =
+        mapOf(
+            RadarDir.TL to V(drone.c.x / 2, drone.c.y / 2),
+            RadarDir.TR to V((SIZE - drone.c.x) / 2 + drone.c.x, drone.c.y / 2),
+            RadarDir.BL to V(drone.c.x / 2, (SIZE - drone.c.y) / 2 + drone.c.y),
+            RadarDir.BR to V((SIZE - drone.c.x) / 2 + drone.c.x, (SIZE - drone.c.y) / 2 + drone.c.y)
+        )
+    return blips.filter { it.droneId == drone.id }.map { coordsByDir[it.dir]!! to it.creatureId }
 }
 
 private fun <T> closestBy(drone: Drone, others: List<T>, selector: (T) -> V): T? {
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,10 +1,37 @@
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import kotlin.test.assertNotNull
 
 internal class MainTest {
 
     @Test
-    fun closest() {
+    fun blipCoords_tl() {
+        assertEquals(
+            V(500, 500) to 2,
+            blipCoords(Drone(1, V(1000, 1000), 0, 0), listOf(Blip(1, 2, RadarDir.TL)))[0]
+        )
+    }
 
+    @Test
+    fun blipCoords_tr() {
+        assertEquals(
+            V(5500, 500) to 2,
+            blipCoords(Drone(1, V(1000, 1000), 0, 0), listOf(Blip(1, 2, RadarDir.TR)))[0]
+        )
+    }
+
+    @Test
+    fun blipCoords_bl() {
+        assertEquals(
+            V(500, 5500) to 2,
+            blipCoords(Drone(1, V(1000, 1000), 0, 0), listOf(Blip(1, 2, RadarDir.BL)))[0]
+        )
+    }
+
+    @Test
+    fun blipCoords_br() {
+        assertEquals(
+            V(5500, 5500) to 2,
+            blipCoords(Drone(1, V(1000, 1000), 0, 0), listOf(Blip(1, 2, RadarDir.BR)))[0]
+        )
     }
 }
